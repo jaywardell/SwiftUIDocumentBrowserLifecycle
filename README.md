63495b279a23e43736c06008613cfae62d3c9fc1
Jay Wardell <jaywardell@me.com>
2023-06-10 09:06:58 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftUIDocumentBrowserLifecycle
+
+A description of this package.
---
+++ Sources/SwiftUIDocumentBrowserLifecycle/SwiftUIDocumentBrowserLifecycle.swift
@@ -0,0 +1,6 @@
+public struct SwiftUIDocumentBrowserLifecycle {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
